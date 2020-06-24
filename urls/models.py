fb2b2e424dcabd7837cec8a87b0a80308a5dc511
amrhgh <amirhgholamian78@gmail.com>
2020-06-24 17:15:46 +0430

    make short url path field unique

--- urls/models.py
+++ urls/models.py
@@ -5,10 +5,11 @@ from users.models import CustomUser
 
 SHORT_URL_LENGTH = 64
 
+
 class Url(models.Model):
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     long_url = models.URLField()
-    short_url_path = models.CharField(max_length=SHORT_URL_LENGTH)
+    short_url_path = models.CharField(max_length=SHORT_URL_LENGTH, unique=True)
 
     def __str__(self):
         return f'{self.owner} -- {self.short_url_path}'
