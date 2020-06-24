42989127a3771376e41911265ede1c67d6e4c58a
amrhgh <amirhgholamian78@gmail.com>
2020-06-24 17:09:28 +0430

    create urls model

--- urls/models.py
+++ urls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from users.models import CustomUser
+
+SHORT_URL_LENGTH = 64
+
+class Url(models.Model):
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    long_url = models.URLField()
+    short_url_path = models.CharField(max_length=SHORT_URL_LENGTH)
+
+    def __str__(self):
+        return f'{self.owner} -- {self.short_url_path}'
