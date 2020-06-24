ef39c221b07e949897743ba0099e5f2b43fd13ed
amrhgh <amirhgholamian78@gmail.com>
2020-06-24 16:05:05 +0430

    add get token with email/username and password

--- users/tests.py
+++ users/tests.py
@@ -9,35 +9,7 @@ from users.models import CustomUser
 from users.views import RegisterView, CustomTokenObtainPairView
 
 
-class RegisterTest(TestCase):
-    def setUp(self):
-        self.factory = APIRequestFactory()
-        self.path = '/register/'
-        self.view = RegisterView.as_view()
 
-    def test_register_new_user_not_email_found(self):
-        data = json.dumps({'username': 'foo', 'password': 'fooooooo'})
-        request = self.factory.post(self.path, data=data,
-                                    content_type='application/json')
-        response = self.view(request)
-        self.assertIn('email', response.data)
-
-    def test_register_new_user_with_correct_data(self):
-        data = json.dumps({'email': 'foo@gmail.com', 'username': 'foo', 'password': 'fooooooo'})
-        request = self.factory.post(self.path, data=data,
-                                    content_type='application/json')
-        response = self.view(request)
-        self.assertEqual(201, response.status_code)
-
-    def test_register_new_user_with_pre_existing_email(self):
-        data = json.dumps({'email': 'new@gmail.com', 'username': 'new', 'password': 'foooooooo'})
-        request = self.factory.post(self.path, data=data,
-                                    content_type='application/json')
-        self.view(request)  # create new user
-        request = self.factory.post(self.path, data=data,
-                                    content_type='application/json')
-        response = self.view(request)
-        self.assertIn('email', response.data)
 
 
 class ObtainJWTToken(TestCase):
