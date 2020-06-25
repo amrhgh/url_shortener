2aae40ce5ad940a3439934aa25e84581d66c00dc
amrhgh <amirhgholamian78@gmail.com>
2020-06-25 08:32:09 +0430

    add suggested url validator to serializer

--- urls/serializers.py
+++ urls/serializers.py
@@ -2,16 +2,25 @@ import random
 import string
 
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from url_shortner.settings import SHORT_URL_LENGTH
 from urls.models import Url
 
-ALPHA_NUMBERS = string.ascii_letters + string.digits
+ALPHA_NUMBERS = string.ascii_lowercase + string.digits
 
 
 class UrlSerializer(serializers.ModelSerializer):
     suggested_path = serializers.CharField(max_length=SHORT_URL_LENGTH, write_only=True, required=False)
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    def validate_suggested_path(self, value):
+        value = value.lower()
+        if len(value) != 8:
+            raise ValidationError({'suggested_path': "Ensure this field has no less than 8 characters."})
+        if not set(value).issubset(ALPHA_NUMBERS):
+            raise ValidationError({'suggested_path': "invalid input, only [a-zA-Z0-9] are permitted"})
+        return value
 
     def save(self):
         short_url_path = generate_random_string(self.validated_data.get('suggested_path'))
@@ -26,7 +35,7 @@ class UrlSerializer(serializers.ModelSerializer):
         read_only_fields = ['short_url_path', ]
 
 
-def generate_random_string(suggested_path=None):
+def generate_random_string(suggested_path=None, length=SHORT_URL_LENGTH):
     if suggested_path:
         path = suggested_path
         while True:
@@ -35,13 +44,13 @@ def generate_random_string(suggested_path=None):
             if not obj:
                 # if path is not used before
                 return path
-            index = random.randint(0, SHORT_URL_LENGTH)
+            index = random.randint(0, length)
             new_char = random.choice(ALPHA_NUMBERS)
             path = path[:index] + new_char + path[index:]  # replace a character with random character
 
     else:
         # if user doesn't suggest path a random string is generated
         random_string = ''
-        for _ in range(SHORT_URL_LENGTH):
+        for _ in range(length):
             random_string = random_string + random.choice(ALPHA_NUMBERS)
         return random_string
--- urls/tests/test_url_view.py
+++ urls/tests/test_url_view.py
@@ -23,3 +23,22 @@ class UrlViewTest(TestCase):
         force_authenticate(request, self.user)
         response = view(request)
         self.assertEqual(201, response.status_code)
+
+    def test_create_new_url_with_suggested_wrong_length(self):
+        view = UrlView.as_view()
+        data = json.dumps({'long_url': 'https://google.com', 'suggested_path': 'short'})
+        request = self.factory.post(self.path, data=data,
+                                    content_type='application/json')
+        force_authenticate(request, self.user)
+        response = view(request)
+        self.assertIn('suggested_path', response.data)
+
+    def test_create_new_url_with_suggested_not_permitted_characters(self):
+        view = UrlView.as_view()
+        data = json.dumps({'long_url': 'https://google.com', 'suggested_path': 'short ur'})
+        request = self.factory.post(self.path, data=data,
+                                    content_type='application/json')
+        force_authenticate(request, self.user)
+        response = view(request)
+        self.assertIn('suggested_path', response.data)
+
