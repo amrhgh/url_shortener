8ce47d8ffa99255a7b24013b9b90cc525515a326
amrhgh <amirhgholamian78@gmail.com>
2020-06-26 00:20:49 +0430

    connect celery to model

--- urls/middleware.py
+++ urls/middleware.py
@@ -14,6 +14,7 @@ class DataStreamQueue:
         user_agent = parse(request.request.META['HTTP_USER_AGENT'])
         user_address = request.request.META['REMOTE_ADDR']
         path = request.kwargs.get('path')
+        print(user_address)
         redis_instance.lpush('data_flow', str({'browser': user_agent.browser.family,
                                                'is_pc': user_agent.is_pc,
                                                'ip': user_address,
