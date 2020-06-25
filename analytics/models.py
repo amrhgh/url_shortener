1c5d4e3d1a68cafb154a86ff1242427373fde934
amrhgh <amirhgholamian78@gmail.com>
2020-06-25 21:55:14 +0430

    create analytics model

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,11 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 # Create your models here.
+from urls.models import Url
+
+
+class Analytic(models.Model):
+    url = models.OneToOneField(to=Url, on_delete=models.CASCADE)
+    records = JSONField()
+    records_unique_user = JSONField()
